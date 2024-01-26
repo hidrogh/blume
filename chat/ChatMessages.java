a85568f98e7df1b24fd9afede1fbdcc5f2df2d49
hidrogh <itshidropm@proton.me>
2024-01-26 09:35:02 +0100

    remove everything for new start

--- chat/ChatMessages.java
+++
@@ -1,9 +0,0 @@
-package blume_system.chat;
-
-import org.bukkit.entity.Player;
-
-public class ChatMessages {
-	public static void notFound(Player p) {
-		p.sendMessage(ChatTags.logBlume() + "Command not found. Please enter '/help' for more information.");
-	}
-}
